95c9c9044424d25f940819d27790b12407286e79
Max Pou Smith <maxpousmith10@gmail.com>
2023-04-16 12:32:06 +0200

    Add nextGeneration logic

--- app/index.js
+++ app/index.js
@@ -1,8 +1,16 @@
+import nextGeneration from "./functions/nextGeneration.js";
 import startGame from "./functions/startGame.js";
 
-const allSquares = Array.from(document.querySelectorAll("td"));
+const squares = Array.from(document.querySelectorAll("td"));
+let squaresLife = [];
+for (let i = 0; i < 100; i++) {
+  squaresLife.push(false);
+}
 
 const button = document.querySelector(".start");
 button.addEventListener("click", () => {
-  startGame(allSquares);
+  startGame(squares, squaresLife);
+  setInterval(() => {
+    squaresLife = nextGeneration(squares, squaresLife);
+  }, 1000);
 });
